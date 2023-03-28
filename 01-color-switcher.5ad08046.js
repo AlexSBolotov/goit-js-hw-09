9a1ccaef2fec176b566be02dd700c453c85baa8b
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-03-28 20:27:51 +0000

    Deploying to gh-pages from @ AlexSBolotov/goit-js-hw-09@e343dddb66ee99527225757f88f25839afab91ca 🚀

---
+++ 01-color-switcher.5ad08046.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.body};let n=null;function o(){const n=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=n}t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){n=setInterval(o,1e3),t.stopBtn.disabled=!1,t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.5ad08046.js.map
