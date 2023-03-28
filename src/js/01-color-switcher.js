e343dddb66ee99527225757f88f25839afab91ca
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-03-28 23:27:24 +0300

    task_01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
 
+let timerId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(setBgc, 1000);
+  refs.stopBtn.disabled = false;
+  refs.startBtn.disabled = true;
+}
+function onStopBtnClick() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function setBgc() {
+  const colorToApply = getRandomHexColor();
+  refs.body.style.backgroundColor = colorToApply;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
